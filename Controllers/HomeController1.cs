9a87733c8a75a5d04385d3bb8d5504e5addfda9d
catalinaaamarian <138561267+catalinaaamarian@users.noreply.github.com>
2025-12-02 18:11:56 +0200

    2

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marian_Catalina_Lab1.Controllers
+{
+    public class HomeController1 : Controller
+    {
+ 
+        public string Index()
+        {
+            return "Welcome to our application! We're glad to have you here.";
+        }
+
+        public string Greeting()
+        {
+            return "Hello! Thank you for visiting our website.";
+        }
+
+    
+        public string DisplayUserInfo(string userName, int userId)
+        {
+            return $"User Information - Name: {userName}, User ID: {userId}";
+        }
+    }
+}
